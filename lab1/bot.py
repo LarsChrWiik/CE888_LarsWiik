9d47f9175767233a001768dd54d0e6123685038f
Lars Christian Wiik <lars_christian_wiik@hotmail.com>
2018-01-22 18:21:59 +0000

    Done with Assignment lab1 - Lars Wiik.

--- lab1/bot.py
+++ lab1/bot.py
@@ -4,6 +4,10 @@ from emo.wdemotions import EmotionDetector
 
 emo = EmotionDetector()
 
-@module.rule('a')
+@module.rule('')
 def hi(bot, trigger):
     print(trigger)
+    array = emo.detect_emotion_in_raw_np(trigger)
+    array = array.tolist()
+    array = [int(i) for i in array]
+    bot.say(str(array))
