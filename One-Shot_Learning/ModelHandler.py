7cdf680c7792d65d05412e25995ed21327088785
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-04-08 21:16:29 +0100

    Added comments.

--- One-Shot_Learning/ModelHandler.py
+++ One-Shot_Learning/ModelHandler.py
@@ -1,29 +1,41 @@
 
 from PredictionModel import PredictionModel
-from Sampler import Sampler
+import Sampler
 import Dataset
 import pickle
 
 
-"""
-Get the path for a model.
-"""
 def get_model_string(count):
+    """
+    Get the path for a model.
+    """
     return "./models/trained_model" + str(count) + ".pkl"
 
 
 def train_model(clf, count, verbose=False):
+    """
+    Train a model.
+
+    :param clf: classifier in sklearn format.
+    :param count: int representing training count.
+    :param verbose: bool representing whether information should be shown.
+    :return: the trained classifier object.
+    """
     if verbose: print("Generate training samples")
-    X_train, Y_train = Sampler.get_samples(path=Dataset.data_background, count=count)
+    X_train, Y_train = Sampler.get_samples(dataset=Dataset.data_background, count=count)
     if verbose: print("Fit")
     clf.fit(X_train, Y_train)
     return clf
 
 
-"""
-Train and save a model to disk. 
-"""
 def train_save_model(clf, count, verbose=False):
+    """
+    Train and save a model to disk.
+
+    :param clf: PredictionModel.
+    :param count: int representing training count.
+    :param verbose: bool representing whether information should be shown.
+    """
     clf = train_model(clf=clf, count=count, verbose=True)
     # Save the model to disk.
     path = get_model_string(count=count)
@@ -31,10 +43,14 @@ def train_save_model(clf, count, verbose=False):
     if verbose: print("model saved: " + path)
 
 
-"""
-Load and pre-trained model from disk. 
-"""
 def load_model(count, verbose=False):
+    """
+    Load and pre-trained model from disk.
+
+    :param count: int representing training count.
+    :param verbose: bool representing whether information should be shown.
+    :return: PredictionModel object containing a trained classifier.
+    """
     # Load the model from disk.
     path = get_model_string(count=count)
     loaded_model = pickle.load(open(path, 'rb'))
@@ -44,9 +60,12 @@ def load_model(count, verbose=False):
     return clf
 
 
-"""
-Continue training a model. 
-"""
 def train_save_new_model(count, verbose=False):
+    """
+    Continue training a model.
+
+    :param count: int representing training count.
+    :param verbose: bool representing whether information should be shown.
+    """
     clf = PredictionModel()
     train_save_model(clf=clf, count=count, verbose=verbose)
