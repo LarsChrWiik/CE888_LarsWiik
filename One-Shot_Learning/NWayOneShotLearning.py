567aab64fa3c7b5d30fd66b0820a06ab65a19e5a
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-04-12 18:00:27 +0100

    Added Interpretability function.

--- One-Shot_Learning/NWayOneShotLearning.py
+++ One-Shot_Learning/NWayOneShotLearning.py
@@ -1,8 +1,12 @@
 
 from Progressbar import show_progressbar
-from PredictionModel import PredictionModel
 import Sampler
 import random
+import ImageHandler
+import math
+from PIL import Image
+import os
+from shutil import rmtree
 
 
 def get_index_of_min_value(a_list):
@@ -28,26 +32,36 @@ def get_index_of_min_value(a_list):
     return min_indexes[0]
 
 
-def predict_most_similar_image(clf, image_main, X):
+def predict(clf, image_main, X):
     """
     Predict single class.
 
     :param clf: classifier.
     :param image_main: list representing an image.
     :param X: list of images.
-    :return: list of numbers representing a single class prediction.
+    :return: list of float representing the prediction.
     """
     predictions = []
     for image in X:
         pred = clf.predict([[image_main, image]])[0]
         predictions.append(pred)
+    return predictions
+
+
+def transform_to_signle_prediction(predictions):
+    """
+    Transform a float prediction into a 1-class prediction.
+
+    :param predictions: list of float representing the prediction.
+    :return: list of int representing a single class prediction.
+    """
     min_index = get_index_of_min_value(predictions)
     predictions = [1 for _ in range(len(predictions))]
     predictions[min_index] = 0
     return predictions
 
 
-def n_way_one_shot_learning(clf, count, dataset, n, verbose=False):
+def n_way_one_shot_learning(clf, count, dataset, n, verbose=False, interpretability=False):
     """
     Implementation of N-way one-shot learning generation.
 
@@ -55,18 +69,56 @@ def n_way_one_shot_learning(clf, count, dataset, n, verbose=False):
     :param count: int representing training count.
     :param dataset: string of the dataset.
     :param n: number of other images in the n-way one shot test.
-    :param verbose:
+    :param verbose: bool representing whether information should be shown.
+    :param interpretability: bool indicating if images should be saved to disk for interpretability reasons.
     :return: float representing the final score of the test.
     """
     prefix = str(n)+"-way one-shot learning: "
     if verbose: show_progressbar(i=0, max_i=count, prefix=prefix)
 
+    if interpretability: __remove_interpret_folder()
+
     correct = 0
     for i in range(count):
         image_main, X, Y = Sampler.n_way_one_shot_learning(dataset=dataset, n=n)
-        prediction = predict_most_similar_image(clf=clf, image_main=image_main, X=X)
+        prediction = predict(clf=clf, image_main=image_main, X=X)
+
+        if interpretability and (prediction == Y and correct < 10) or (prediction != Y and i-correct < 10):
+            __save_interpret_image(image=image_main, iteration=i, is_main=True)
+            for image in enumerate(X):
+                __save_interpret_image(
+                    image=image,
+                    iteration=i,
+                    is_correct=prediction == Y,
+                    score=prediction[i]
+                )
+
+        prediction = transform_to_signle_prediction(prediction)
+
         if prediction == Y: correct += 1
         if verbose: show_progressbar(i=i+1, max_i=count, prefix=prefix)
     if verbose: show_progressbar(i=count, max_i=count, prefix=prefix, finish=True)
 
     return (correct*100) / count
+
+
+def __remove_interpret_folder():
+    rmtree("./interpretability/")
+
+
+def __save_interpret_image(image, iteration, is_correct=None, score=None, is_main=False):
+    img = ImageHandler.ensure_1D_image(image)
+    size = int(math.sqrt(len(img)))
+    img2 = Image.new("1", (size, size))
+    img2.putdata(img)
+
+    correct_folder = "correct" if is_correct else "wrong"
+    name = str(round(score, 4))
+    path = "./interpretability/" + str(iteration) + "/" + str(correct_folder)
+    if is_main:
+        path = "./interpretability/" + str(iteration)
+        name = "main_image"
+
+    if not os.path.exists(path): os.makedirs(path)
+
+    img2.save(path + "/" + name + '.png')
