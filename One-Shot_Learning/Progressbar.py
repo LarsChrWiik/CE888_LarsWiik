1156286ba7433814cecaad44abe4ba41059d7fe1
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-04-15 18:29:52 +0100

    Added comments.

--- One-Shot_Learning/Progressbar.py
+++ One-Shot_Learning/Progressbar.py
@@ -5,6 +5,14 @@ import sys
 Display progressbar. 
 """
 def show_progressbar(i, max_i, prefix="", finish=False):
+    """
+    Displays a progressbar.
+
+    :param i: int, iteration number.
+    :param max_i: int, maximum iteration number.
+    :param prefix: string, any words before the score is displayed.
+    :param finish: bool, indicating if this is the last message of the progress bar.
+    """
     sys.stdout.write("\r")
     sys.stdout.write(prefix + str(i * 100 / max_i) + "%")
     sys.stdout.flush()
